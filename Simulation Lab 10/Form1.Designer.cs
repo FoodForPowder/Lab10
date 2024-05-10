017ef648dc236c4af221d579e511a3d0173f0aae
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-05-10 15:44:35 +0700

    Добавьте файлы проекта.

---
+++ Simulation Lab 10/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Simulation_Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(215, 384);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 106);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 4;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(518, 248);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Результаты этапа турнира по футболу";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(600, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Simulation Lab 10/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Comparer : IComparer<Team>
+        {
+            public int Compare(Team x, Team y)
+            {
+                return x.points.CompareTo(y.points);
+            }
+        }
+
+        public class Team
+        {
+            public string t;
+            public double l;
+            public int points;
+
+            public Team()
+            {
+                t = "";
+                l = 0;
+                points = 0;
+            }
+        }
+
+        int round = 0;
+        Team[] teams = new Team[8];
+        List<Team> summary = new List<Team>();
+        Team team = new Team(), team1 = new Team();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++) { teams[i] = new Team(); }
+
+            teams[0].t = "Локомотив"; teams[0].l = 1.5;
+            teams[1].t = "ЦСКА"; teams[1].l = 1.9;
+            teams[2].t = "Спартак"; teams[2].l = 1.1;
+            teams[3].t = "Зенит"; teams[3].l = 0.7;
+            teams[4].t = "Динамо"; teams[4].l = 1.4;
+            teams[5].t = "Урал"; teams[5].l = 0.9;
+            teams[6].t = "Ахмат"; teams[6].l = 0.5;
+            teams[7].t = "Арсенал"; teams[7].l = 0.8;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            summary.Clear();
+
+            var restT = new List<Team>(teams.Skip(1));
+            int teamsCount = 8;
+
+            if (restT[round % restT.Count]?.Equals(default) == false)
+            {
+                int m = 0;
+                double S = 0;
+                double a = rand.NextDouble();
+                S += Math.Log(a);
+                while (S >= -teams[0].l)
+                {
+                    m++;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                }
+                teams[0].points += m;
+                summary.Add(teams[0]);
+
+                m = 0;
+                S = 0;
+                a = rand.NextDouble();
+                S += Math.Log(a);
+                while (S >= -restT[round % restT.Count].l)
+                {
+                    m++;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                }
+                restT[round % restT.Count].points += m;
+                summary.Add(restT[round % restT.Count]);
+            }
+
+            for (var index = 1; index < teamsCount / 2; index++)
+            {
+                var first = restT[(round + index) % restT.Count];
+                var second = restT[(round + restT.Count - index) % restT.Count];
+                if (first?.Equals(default) == false && second?.Equals(default) == false)
+                {
+                    int m = 0;
+                    double S = 0;
+                    double a = rand.NextDouble();
+                    S += Math.Log(a);
+                    while (S >= -first.l)
+                    {
+                        m++;
+                        a = rand.NextDouble();
+                        S += Math.Log(a);
+                    }
+                    first.points += m;
+                    summary.Add(first);
+
+                    m = 0;
+                    S = 0;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                    while (S >= -second.l)
+                    {
+                        m++;
+                        a = rand.NextDouble();
+                        S += Math.Log(a);
+                    }
+                    second.points += m;
+                    summary.Add(second);
+                }
+            }
+
+            Comparer comp = new Comparer();
+            summary.Sort(comp);
+            summary.Reverse();
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].HeaderText = "Позиция";
+            dataGridView1.Columns[1].HeaderText = "Команда";
+            dataGridView1.Columns[2].HeaderText = "Очки";
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (round == 0) dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = i + 1;
+                dataGridView1.Rows[i].Cells[1].Value = summary[i].t;
+                dataGridView1.Rows[i].Cells[2].Value = summary[i].points;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e) {}
+    }
+}
+
+
+
+
